3cf2add77eaccba686ffcaa00ccfc0b2e9fa1774
GAujla <G.S.Aujla@outlook.com>
2023-05-25 23:05:46 +0100

    relevent info wrote to dict

--- new_results.py
+++ new_results.py
@@ -1,35 +1,45 @@
-import requests
 import csv
+import requests
 
+header_list = ['Driver']
+driver_data = {}  # Dictionary to store driver names and positions
+print(driver_data)
 
-header_list = []
 for i in range(1, 6):
     base_url = f'http://ergast.com/api/f1/current/{i}/results.json'
-
     response = requests.get(base_url)
 
     if response.status_code == 200:
         data = response.json()
-        races = data['MRData']['RaceTable']['Races']            
-        race_names = [race['raceName'] for race in races]
-        tracks = races[0]['raceName']
-        header_list.append(tracks)
-        with open("gfg3.csv", 'w') as file:
-            dw = csv.DictWriter(file, delimiter=',', 
-                        fieldnames=header_list)
-            dw.writeheader()
+        races = data['MRData']['RaceTable']['Races']
+        track = races[0]['raceName']
+        header_list.append(track)
 
-        
-        print(header_list)
-
-        
-    
-
-
-            
         results = races[0]['Results']
         for result in results:
             driver = result['Driver']['givenName']
             position = result['position']
+            if driver in driver_data:
+                driver_data[driver].append(position)
+            else:
+                driver_data[driver] = [position]
+                print(driver_data)
+
     else:
         print(f'Error: {response.status_code}')
+
+driver_names = list(driver_data.keys())
+
+# Write the driver names and positions to CSV
+with open("f1.csv", 'w') as file:
+    dw = csv.DictWriter(file, delimiter=',', fieldnames=header_list)
+    dw.writeheader()
+
+    for driver_name in driver_names:
+        driver_positions = driver_data[driver_name]
+        row = {'Driver': driver_name}
+
+        for i, position in enumerate(driver_positions):
+            row[header_list[i+1]] = position
+
+        dw.writerow(row)
