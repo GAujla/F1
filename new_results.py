e166ed878aa3eea74bff2fb9e5f4c6f0502e6242
GAujla <G.S.Aujla@outlook.com>
2023-05-31 20:25:39 +0100

    Refactored code

--- new_results.py
+++ new_results.py
@@ -1,45 +1,55 @@
 import csv
 import requests
 
-header_list = ['Driver']
-driver_data = {}  # Dictionary to store driver names and positions
-print(driver_data)
+class Driver_Information:
+    def __init__(self, base_url):
+        self.base_url = base_url
+        self.header_list = ['Driver']
+        self.driver_data = {}
 
-for i in range(1, 6):
-    base_url = f'http://ergast.com/api/f1/current/{i}/results.json'
-    response = requests.get(base_url)
+    def ReadingData(self):
+        self.header_list = ['Driver']
+        self.driver_data = {}  # Dictionary to store driver names and positions
 
-    if response.status_code == 200:
-        data = response.json()
-        races = data['MRData']['RaceTable']['Races']
-        track = races[0]['raceName']
-        header_list.append(track)
+        for i in range(1, 6):
+            url = self.base_url.format(i=i)  # Update the URL with the correct value of 'i'
 
-        results = races[0]['Results']
-        for result in results:
-            driver = result['Driver']['givenName']
-            position = result['position']
-            if driver in driver_data:
-                driver_data[driver].append(position)
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = response.json()
+                races = data['MRData']['RaceTable']['Races']
+                track = races[0]['raceName']
+                self.header_list.append(track)   
+
+                results = races[0]['Results']
+                for result in results:
+                    driver = result['Driver']['givenName']
+                    position = result['position']
+                    if driver in self.driver_data:
+                        self.driver_data[driver].append(position)
+                    else:
+                        self.driver_data[driver] = [position]
+
             else:
-                driver_data[driver] = [position]
-                print(driver_data)
+                print(f'Error: {response.status_code}')
 
-    else:
-        print(f'Error: {response.status_code}')
 
-driver_names = list(driver_data.keys())
+    def write_to_csv(self):
+        driver_names = list(self.driver_data.keys())
+        # Write the driver names and positions to CSV
+        with open("f1.csv", 'w') as file:
+            dw = csv.DictWriter(file, delimiter=',', fieldnames=self.header_list)
+            dw.writeheader()
 
-# Write the driver names and positions to CSV
-with open("f1.csv", 'w') as file:
-    dw = csv.DictWriter(file, delimiter=',', fieldnames=header_list)
-    dw.writeheader()
+            for driver_name in driver_names:
+                driver_positions = self.driver_data[driver_name]
+                row = {'Driver': driver_name}
 
-    for driver_name in driver_names:
-        driver_positions = driver_data[driver_name]
-        row = {'Driver': driver_name}
+                for i, position in enumerate(driver_positions):
+                    row[self.header_list[i+1]] = position
 
-        for i, position in enumerate(driver_positions):
-            row[header_list[i+1]] = position
+                dw.writerow(row)
 
-        dw.writerow(row)
+csv_writer = Driver_Information('http://ergast.com/api/f1/current/{i}/results.json')
+csv_writer.ReadingData()
+csv_writer.write_to_csv()
